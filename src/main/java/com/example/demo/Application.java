0d1e7ecb0f0c5ac87e444b3e69f138683cf39ac2
Sirma-cloud <kevinkiplimo99@gmail.com>
2023-08-01 12:54:44 +0300

    One-to-many relation using the foreign key in the book entity

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.IntStream;
 
@@ -35,39 +36,70 @@ public class Application {
                     email,
                     faker.number().numberBetween(17, 55));
 
+
+            student.addBook(
+                    new Book("Think like a man", LocalDateTime.now().minusDays(4)));
+
+
+            student.addBook(
+                    new Book("Think and Grow Rich", LocalDateTime.now()));
+
+
+            student.addBook(
+                    new Book("Spring Data JPA", LocalDateTime.now().minusYears(1)));
+
             StudentIdCard studentIdCard = new StudentIdCard(
                     "123456789",
                     student);
 
-            studentIdCardRepository.save(studentIdCard);
+            student.setStudentIdCard(studentIdCard);
+
+            studentRepository.save(student);
 
             studentRepository.findById(1L)
-                    .ifPresent(System.out::println);
+                    .ifPresent(s -> {
+                        System.out.println("fetch book lazy....");
+                        List<Book> books = student.getBooks();
+                        books.forEach(book -> {
+                            System.out.println(
+                                    s.getFirstName() + " borrowed " + book.getBookName());
+                        });
+                    });
 
-            studentIdCardRepository.findById(1L)
-                    .ifPresent(System.out::println);
-
+//            StudentIdCard studentIdCard = new StudentIdCard(
+//                    "123456789",
+//                    student);
+//
+//            studentIdCardRepository.save(studentIdCard);
+//
+//            studentRepository.findById(1L)
+//                    .ifPresent(System.out::println);
+//
+//            studentIdCardRepository.findById(1L)
+//                    .ifPresent(System.out::println);
+//
 //            studentRepository.deleteById(1L);
+
 
         };
     }
-
-    private void generateRandomStudents(StudentRepository studentRepository) {
-        Faker faker = new Faker();
-        for (int i = 0; i < 20; i++) {
-            String firstName = faker.name().firstName();
-            String lastName = faker.name().lastName();
-            String email = String.format("%s.%s@gmail.com", firstName, lastName);
-            Student student = new Student(
-                    firstName,
-                    lastName,
-                    email,
-                    faker.number().numberBetween(17, 55));
-            studentRepository.save(student);
-        }
-    }
-
 }
+
+//    private void generateRandomStudents(StudentRepository studentRepository) {
+//        Faker faker = new Faker();
+//        for (int i = 0; i < 20; i++) {
+//            String firstName = faker.name().firstName();
+//            String lastName = faker.name().lastName();
+//            String email = String.format("%s.%s@gmail.com", firstName, lastName);
+//            Student student = new Student(
+//                    firstName,
+//                    lastName,
+//                    email,
+//                    faker.number().numberBetween(17, 55));
+//            studentRepository.save(student);
+//        }
+//    }
+//}
 
 
 
